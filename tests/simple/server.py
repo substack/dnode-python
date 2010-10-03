32129974107d86c4efd5ee873d360882ebabfe88
Joshua Holbrook <josh.holbrook@gmail.com>
2010-10-03 02:58:55 -0800

    Minor movement of comment in server example

--- tests/simple/server.py
+++ tests/simple/server.py
@@ -2,13 +2,12 @@
 
 from dnode import DNode, DNode_decs
 
+#ugly.
 class For_DNode(object):
     def timesTen(self, n, reply):
         reply(n*10)
     def moo(self, reply):
         reply(100)
-
-#ugly.
 DNode(For_DNode()).listen(12321)
 
 #Can this be done with a decorator? Say: 
